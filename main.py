d59cffdde448b539af547f594cd39a424ec28a46
Keshav K Das <94295810+keshavkdas@users.noreply.github.com>
2025-07-06 12:24:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from input.text_input import get_command
+from output.speak import respond
+
 def main():
-    print("Hello, I am your JARVIS. Awaiting your command...")
+    respond("Hello, I am your JARVIS. Awaiting your command...")
+    while True:
+        command = get_command().lower()
+        if command in ["exit", "quit", "bye"]:
+            respond("Goodbye!")
+            break
+        else:
+            respond(f"You said: {command}")
 
 if __name__ == "__main__":
     main()
